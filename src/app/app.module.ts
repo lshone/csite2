1cc363824d06fe63d08516f017f8a9e872c19cca
Laurie Shone <shoner28@gmail.com>
2021-06-08 11:46:21 -0600

    create Post foundation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
-import { CoreModule } from './core/core.module';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
+import { AppComponent } from './app.component';
 
+import { MaterialModule } from './material.module';
+import { CoreModule } from './core/core.module';
+import { PostModule } from './post/post.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { environment } from 'src/environments/environment';
     MaterialModule,
     CoreModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    PostModule
   ],
   providers: [],
   bootstrap: [AppComponent]
